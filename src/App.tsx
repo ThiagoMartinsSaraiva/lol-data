a1c627d091e82de0e27ff32c1d7c081921244f1d
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2022-03-24 01:19:56 -0300

    feat: initializing the modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,83 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { useVersion } from './contexts/version'
-import { getAll } from './services/champion'
+import { getAll, getChampion } from './services/champion'
 
-interface Champion {
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
+
+interface ListItemChampion {
+  name: string
+  key: string
+  image: ListItemChampionImage
+}
+
+interface ListItemChampionImage {
+  full: string
+}
+
+interface ChampionList {
+  [champion: string]: ListItemChampion
+}
+
+interface CurrentChampion {
   name: string
   key: string
   title: string
-  partype: string
-  blurb: string
-  image: ChampionImage
-  info: ChampionInfo
+  skins: CurrentChampionSkins[]
+  allytips: string[]
+  enemytips: string[]
   tags: string[]
+  partype: string
+  info: CurrentChampionStats
+  passive: Omit<CurrentChampionSpells, 'id' | 'cooldown' | 'cost'>
+  spells: CurrentChampionSpells[]
+  image: {
+    full: string
+  }
+}
+interface CurrentChampionSkins {
+  id: string
+  num: number
+  name: string
+  chromas: boolean
 }
 
-interface ChampionImage {
-  full: string
-  sprite: string
-  group: string
-  x: number
-  y: number
-  w: number
-  h: number
-}
-
-interface ChampionInfo {
+interface CurrentChampionStats {
   attack: number
   defense: number
   magic: number
   difficulty: number
 }
 
-interface ChampionKey {
-  [champion: string]: Champion
+interface CurrentChampionSpells {
+  id: string
+  name: string
+  description: string
+  cooldown: number[]
+  cost: number[]
+  image: CurrentChampionSpellImage
+}
+
+interface CurrentChampionSpellImage {
+  full: string
+  sprite: string
+}
+
+const skillIndexToKeyboardCode: { [key: number]: string } = {
+  0: 'Q',
+  1: 'W',
+  2: 'E',
+  3: 'R',
 }
 
 function App() {
-  const [champions, setChampions] = useState<ChampionKey>({} as ChampionKey)
+  const [champions, setChampions] = useState<ChampionList>({} as ChampionList)
+  const [isChampionModalOpen, setIsChampionModalOpen] = useState(false)
+  const [currentChampion, setCurrentChampion] = useState<CurrentChampion>(
+    {} as CurrentChampion,
+  )
   const { version } = useVersion()
 
   useEffect(() => {
@@ -47,11 +88,28 @@ function App() {
     }
   }, [version])
 
+  function handleOpenChampionModal(championName: string) {
+    getChampion({ version, locale: 'pt_BR', championName }).then((data) => {
+      Object.keys(data).map((key) => {
+        setCurrentChampion(data[key])
+      })
+    })
+    setIsChampionModalOpen(true)
+  }
+
+  function handleCloseChampionModal() {
+    setIsChampionModalOpen(false)
+  }
+
   return (
     <>
       <section className="champions-container">
         {Object.keys(champions).map((championKey) => (
-          <div key={championKey} className="champion-card">
+          <div
+            key={championKey}
+            className="champion-card"
+            onClick={() => handleOpenChampionModal(championKey)}
+          >
             <div className="image-container">
               <img
                 src={`https://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${champions[championKey].image.full}`}
@@ -64,6 +122,119 @@ function App() {
           </div>
         ))}
       </section>
+      <Modal
+        isOpen={isChampionModalOpen}
+        onRequestClose={handleCloseChampionModal}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+      >
+        {Object.keys(currentChampion).length > 0 && (
+          <>
+            <div className="modal-header">
+              <div className="champion-profile-image">
+                <img
+                  src={`https://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${currentChampion.image.full}`}
+                  loading="lazy"
+                />
+              </div>
+            </div>
+            <div className="modal-body">
+              {Object.keys(currentChampion).length > 0 && (
+                <>
+                  <p>{currentChampion.name}</p>
+                  <div>
+                    <div>
+                      <p>Classes</p>
+                      <p>
+                        {currentChampion.tags.map((tag) => (
+                          <span key={tag}>{tag}</span>
+                        ))}
+                      </p>
+                    </div>
+                  </div>
+                  <div>
+                    <div>
+                      <p>Info</p>
+                      <p>{currentChampion.info.attack}</p>
+                      <p>{currentChampion.info.defense}</p>
+                      <p>{currentChampion.info.difficulty}</p>
+                      <p>{currentChampion.info.magic}</p>
+                    </div>
+                    <div>
+                      <p>Spells</p>
+                      <div>
+                        <p>{currentChampion.passive.name}</p>
+                        <img
+                          src={`https://ddragon.leagueoflegends.com/cdn/12.5.1/img/passive/${currentChampion.passive.image.full}`}
+                          loading="lazy"
+                        />
+                        <p>{currentChampion.passive.description}</p>
+                        <div>
+                          <video
+                            src={`https://d28xe8vt774jo5.cloudfront.net/champion-abilities/${currentChampion.key.padStart(
+                              4,
+                              '0',
+                            )}/ability_${currentChampion.key.padStart(
+                              4,
+                              '0',
+                            )}_P1.mp4`}
+                            controls
+                            preload="metadata"
+                            muted={true}
+                            style={{ width: '20%' }}
+                          />
+                        </div>
+                      </div>
+                      {currentChampion.spells.map((spell, index) => (
+                        <div key={spell.name}>
+                          <p>{spell.name}</p>
+                          <img
+                            src={`https://ddragon.leagueoflegends.com/cdn/12.5.1/img/spell/${spell.image.full}`}
+                            loading="lazy"
+                          />
+                          <p>{spell.description}</p>
+                          <div>
+                            <video
+                              src={`https://d28xe8vt774jo5.cloudfront.net/champion-abilities/${currentChampion.key.padStart(
+                                4,
+                                '0',
+                              )}/ability_${currentChampion.key.padStart(
+                                4,
+                                '0',
+                              )}_${skillIndexToKeyboardCode[index]}1.mp4`}
+                              controls
+                              preload="metadata"
+                              muted={true}
+                              style={{ width: '20%' }}
+                            />
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                    <div>
+                      <p>Skins</p>
+                      {currentChampion.skins.map(
+                        (skin: CurrentChampionSkins) => (
+                          <div key={skin.id}>
+                            <img
+                              src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${currentChampion.name}_${skin.num}.jpg`}
+                              loading="lazy"
+                              className="image-splash-art"
+                            />
+                            {skin.name === 'default'
+                              ? currentChampion.name
+                              : skin.name}
+                          </div>
+                        ),
+                      )}
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+          </>
+        )}
+      </Modal>
     </>
   )
 }
--- src/services/champion.ts
+++ src/services/champion.ts
@@ -1,11 +1,28 @@
 import { api } from '../api'
 
-type props = {
+interface GetAllProps {
   version: string
   locale: string
 }
 
-export async function getAll({ version, locale }: props) {
+interface GetChampionProps {
+  version: string
+  locale: string
+  championName: string
+}
+
+export async function getAll({ version, locale }: GetAllProps) {
   const { data } = await api.get(`/cdn/${version}/data/${locale}/champion.json`)
+  return data.data
+}
+
+export async function getChampion({
+  version,
+  locale,
+  championName,
+}: GetChampionProps) {
+  const { data } = await api.get(
+    `https://ddragon.leagueoflegends.com/cdn/${version}/data/${locale}/champion/${championName}.json`,
+  )
   return data.data
 }
